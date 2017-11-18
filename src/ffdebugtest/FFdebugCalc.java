67a2ce8872c836ec1465ce08007e21812a10b0b8
Pierre <jouannet.pierre@gmail.com>
2017-11-18 20:44:38 +0100

    Détails mineurs.

--- src/ffdebugtest/FFdebugCalc.java
+++ src/ffdebugtest/FFdebugCalc.java
@@ -1,7 +1,16 @@
 package ffdebugtest;
 
+/**
+ * Classe Java contenant les méthodes de comparaison des entiers dans une liste.
+ * @author Pierre
+ */
 public class FFdebugCalc {
     
+    /**
+     * Trouve l'integer le plus grand dans une liste d'integer.
+     * @param array liste d'integer.
+     * @return integer le plus grand contenu dans la liste.
+     */
     public static int findMax(int[] array) {
         int max = array[0];
         for (int i = 0; i < array.length; i++) {
@@ -12,6 +21,11 @@ public class FFdebugCalc {
         return max;
     }  
     
+    /**
+     * Trouve l'integer le plus petit dans une liste d'integer.
+     * @param array liste d'integer.
+     * @return integer le plus petit contenu dans la liste.
+     */
     public static int findMin(int[] array) {
         int min = array[0];
         for (int i = 0; i < array.length; i++) {
