a8600c69d0ebf73be634401e0e2fccdbee6ba488
Pierre <jouannet.pierre@gmail.com>
2017-11-18 20:42:05 +0100

    Ajout des commentaire pour la JavaDoc.

--- src/ffdebugtest/FFdebugTest.java
+++ src/ffdebugtest/FFdebugTest.java
@@ -1,19 +1,34 @@
 package ffdebugtest;
 import java.util.Scanner;
 
+/**
+ * Classe debug pour présenter NetBeans aux M1.
+ * @author Pierre
+ */
 public class FFdebugTest {
     private int[] myArray;
     private int myInt;
     private boolean myBoolean;
     private String myString;
 
+    /**
+     * Constructeur pour la classe FFdebugTest.
+     */
     public FFdebugTest() {
     }
 
+    /**
+     * Second constructeur avec myArray en paramètre
+     * @param myArray Liste de trois entiers.
+     */
     public FFdebugTest(int[] myArray) {
         this.myArray = myArray;
     }
 
+    /**
+     * Exécute les méthodes du programme.
+     * @param args the command line argument
+     */
     public static void main(String[] args) {
         FFdebugTest debug = new FFdebugTest(new int[3]);
         FFdebugTest.getValue(debug.myArray);
@@ -21,6 +36,10 @@ public class FFdebugTest {
         System.out.println("La plus petite valeur est : " + FFdebugCalc.findMin(debug.myArray));
     }
     
+    /**
+     * Permet l'entrée des valeurs d'integer dans myArray par l'utilisateur.
+     * @param array 
+     */
     public static void getValue(int[] array) {
         Scanner scan = new Scanner(System.in);
         for (int i = 0; i < array.length; i++) {
